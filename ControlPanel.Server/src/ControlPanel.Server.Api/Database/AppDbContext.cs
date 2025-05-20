098c3b361852bd77577871969b979d71bb0ec7e1
Denis R. <rda83@mail.ru>
2025-05-20 21:03:06 +0700

    Подключение к БД

---
+++ ControlPanel.Server/src/ControlPanel.Server.Api/Database/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ControlPanel.Server.Api.Database
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
+    }
+}
--- ControlPanel.Server/src/ControlPanel.Server.Api/Program.cs
+++ ControlPanel.Server/src/ControlPanel.Server.Api/Program.cs
@@ -1,4 +1,7 @@
 
+using ControlPanel.Server.Api.Database;
+using Microsoft.EntityFrameworkCore;
+
 namespace ControlPanel.Server.Api
 {
     public class Program
@@ -13,6 +16,9 @@ namespace ControlPanel.Server.Api
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+
+            builder.Services.AddDbContext<AppDbContext>(options =>
+                options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
             var app = builder.Build();
 
