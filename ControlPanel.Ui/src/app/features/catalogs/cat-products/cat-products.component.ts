72f47b80eb7f95dbb63b859f7787c94acc8b6cab
denis <rda198324@google.com>
2025-07-08 08:43:30 +0700

    Datagrid - Custom sort

--- ControlPanel.Ui/src/app/features/catalogs/cat-products/cat-products.component.ts
+++ ControlPanel.Ui/src/app/features/catalogs/cat-products/cat-products.component.ts
@@ -1,8 +1,15 @@
 import { Component } from '@angular/core';
 import { Product } from './models/product';
-import { ClrDatagridModule, ClrIconModule } from '@clr/angular';
+import { ClrDatagridComparatorInterface, ClrDatagridModule, ClrDatagridSortOrder, ClrIconModule } from '@clr/angular';
 import { CommonModule } from '@angular/common';
 import { ProductsService } from './cat-products.service';
+
+
+class ProductComparator implements ClrDatagridComparatorInterface<Product> {
+  compare(a: Product, b: Product) {
+    return a.price - b.price;
+  }
+}
 
 @Component({
   standalone: true,
@@ -14,6 +21,10 @@ import { ProductsService } from './cat-products.service';
 })
 export class CatProductsComponent {
   
+  public productComparator = new ProductComparator();
+
+  descSort = ClrDatagridSortOrder.DESC; //ClrDatagridSortOrder.UNSORTED;
+
   products: Product[];
 
   constructor(public productsService: ProductsService)
