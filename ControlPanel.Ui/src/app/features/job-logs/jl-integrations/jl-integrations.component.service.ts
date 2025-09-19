047ab5d017b421e694d4671c500d510c44c18bb4
Denis Rostovtsev <rda83@mail.ru>
2025-09-19 15:14:05 +0700

    Datagrid

--- ControlPanel.Ui/src/app/features/job-logs/jl-integrations/jl-integrations.component.service.ts
+++ ControlPanel.Ui/src/app/features/job-logs/jl-integrations/jl-integrations.component.service.ts
@@ -3,12 +3,16 @@
 import { HttpClient } from "@angular/common/http";
 import { inject, Injectable } from "@angular/core";
 import { IntegrationEvent } from "./models/integration-event.model";
+import { shareReplay } from "rxjs";
 
 @Injectable()
 export class JournalIntegrationsService {
    
     private http = inject(HttpClient);
     private url = 'http://localhost:5232/api/IntegrationsEvent';
-    readonly integrationEvents$ = this.http.get<IntegrationEvent[]>(this.url);
+    readonly integrationEvents$ = this.http.get<IntegrationEvent[]>(this.url)
+    .pipe(
+        shareReplay(1)
+    );
 }
 
--- ControlPanel.Ui/src/app/features/job-logs/jl-integrations/jl-integrations.component.ts
+++ ControlPanel.Ui/src/app/features/job-logs/jl-integrations/jl-integrations.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { JournalIntegrationsService } from './jl-integrations.component.service';
+import { CommonModule } from '@angular/common';
+import { ClrDatagridModule } from "@clr/angular";
+import { map, startWith } from 'rxjs';
 
 @Component({
   standalone: true,
   selector: 'app-jl-integrations',
-  imports: [],
+  providers:[JournalIntegrationsService],
+  imports: [CommonModule, ClrDatagridModule],
   templateUrl: './jl-integrations.component.html',
   styleUrl: './jl-integrations.component.css'
 })
 export class JlIntegrationsComponent {
-    // Нужна локальная переменная integrationEvents$
+    
+    private service = inject(JournalIntegrationsService);
+    readonly integrationEvents$ = this.service.integrationEvents$;
+    readonly countItems$ = this.service.integrationEvents$
+      .pipe(
+          map(events => events?.length || 0),  
+          startWith(0)
+      ); 
 }
